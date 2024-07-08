5364d469c6a305dfd6f052f5c42c3782becaf006
MaulDavide <maulelladavide@gmail.com>
2024-07-08 13:18:38 +0200

    Actualizacion 3

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -17,157 +17,6 @@ function closeNavAndScrollTo(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
 
-//buscador
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    cartItem.classList.remove('active');
-}
-
-//carrito
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    searchForm.classList.remove('active');
-    }
-    
-/*
-//Añadir productos al carrito
-const btnCart = document.querySelector('')
-const containerCartProducts = document.querySelector('')
-
-btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('')
-})
-
-//Lista de todos los contenedores de productos
-const productsList = document.querySelector('.products')
-
-//Variable de arreglos de Productos
-let allProducts = [];
-
-const valorTotal = document.querySelector('.total-pagar')
-
-const countProducts = document.querySelector('#contador-productos')
-
-productsList.addEventListener('click', e => {
-    if(e.target.classList.contains('btn-add-cart')){
-        const product = e.target.parentElement
-
-        const infoProduct = {
-            quantity: 1,
-            title: product.querySelector('h3').textContent,
-            price: product.querySelector('.price').textContent,
-        };
-
-        const exits = allProducts.some(product => product.title === infoProduct.title)
-
-        if(exits){
-            const products = allProducts.map(product => {
-                if(product.title === infoProduct.title){
-                    product.quantity++;
-                    return product
-                } else{
-                    return product
-                }
-            })
-            allProducts = [...products]
-        }else{
-
-            allProducts = [...allProducts, infoProduct];
-        }
-
-
-        showHTML();
-    }
-});
-
-rowProduct.addEventListener('click', () => {
-    if(e.target.classList.contains('icon-close')){
-        const product = e.target.parentElement
-        const title = product.querySelecor('p').textContent 
-
-        allProducts = allProducts.filter(product => product.title !== title);
-
-        showHTML()
-    }
-})
-
-//Funcion para mostrar HTML
-const showHTML = () => {
-
-    if(!allProducts.length){
-        containerCartProducts.innerHTML=`<p class="cart-empty">El carrito está vacío</p>`
-    }
-
-    //Limpiar HTML
-    rowProduct.innerHTML = '';
-
-    let  total = 0;
-    let totalOfProducts = 0;
-
-    allProducts.forEach(product => {
-        const containerProduct = document.createElement('div')
-        containerProduct.classList.add('cart-product')
-
-        containerProduct.innerHTML = `            
-            <div class="cart-item">
-                <span class="fas fa-times"></span>
-                <img src="./img/pan.avif" alt="">
-                <div class="content">
-                    <h3>${product.title}</h3>
-                    <div class="price">${product.price}</div>
-                </div>
-            </div>
-        `
-
-    rowProduct.append(containerProduct);
-
-    total = total + parseInt(product.quantity * product.price.slice(1))
-    totalOfProducts = totalOfProducts + product.quantity;
-
-    });
-
-    valorTotal.innerText = `€${total}`
-    countProducts.innerText = totalOfProducts;
-};
-*/
-
-
-//scroll
-window.onscroll = () =>{
-//  navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-//Scroll navegador
-let ubicacionPrincipal = window.scrollY;
-let $nav = document.querySelector('#nav');
-
-window.addEventListener('scroll', function() {
-    let ubicacionActual = window.scrollY;
-
-    if(ubicacionPrincipal >= ubicacionActual){
-        $nav.style.top = "0px"
-    } else {
-        $nav.style.top = "-150px"
-    }
-
-    ubicacionPrincipal = ubicacionActual
-});
-
-//Fecha nacimiento actualizable cada día: 120 años
-document.addEventListener('DOMContentLoaded', (event) => {
-    const today = new Date().toISOString().split('T')[0];
-    const minDateString = new Date(new Date().setFullYear(new Date().getFullYear() - 120)).toISOString().split('T')[0];
-
-    document.getElementById('edad').setAttribute('max', today);
-    document.getElementById('edad').setAttribute('min', minDateString);
-});
-
 //Formulario contacto login y registro
 document.addEventListener('DOMContentLoaded', function() {
     const formularioLogin = document.getElementById('formulario-login');
@@ -304,3 +153,211 @@ function listarClientes(){
         });
     }
 }
+
+//buscador
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    login.classList.remove('active');
+    forgot.classList.remove('active')
+    register.classList.remove('active');
+    legal.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+//login
+let login = document.querySelector('.formulario-login');
+
+document.querySelector('#login-btn').onclick = () =>{
+    searchForm.classList.remove('active');
+    login.classList.toggle('active');
+    forgot.classList.remove('active')
+    register.classList.remove('active');
+    legal.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+//forgot
+let forgot = document.querySelector('.formulario-recuperar');
+
+document.querySelector('#login-btn').onclick = () =>{
+    searchForm.classList.remove('active');
+    login.classList.remove('active');
+    forgot.classList.toggle('active')
+    register.classList.remove('active');
+    legal.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+//register
+let register = document.querySelector('.formulario-singUp');
+
+document.querySelector('#login-btn').onclick = () =>{
+    searchForm.classList.remove('active');
+    login.classList.remove('active');
+    forgot.classList.remove('active')
+    register.classList.toggle('active');
+    legal.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+//legal
+let legal = document.querySelector('.legal');
+
+document.querySelector('#login-btn').onclick = () =>{
+    searchForm.classList.remove('active');
+    login.classList.remove('active');
+    forgot.classList.remove('active')
+    register.classList.remove('active');
+    legal.classList.toggle('active');
+    cartItem.classList.remove('active');
+}
+
+//carrito
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    searchForm.classList.remove('active');
+    login.classList.remove('active');
+    forgot.classList.remove('active')
+    register.classList.remove('active');
+    legal.classList.remove('active');
+    cartItem.classList.toggle('active');
+    }
+
+    
+/*
+//Añadir productos al carrito
+const btnCart = document.querySelector('')
+const containerCartProducts = document.querySelector('')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('')
+})
+
+//Lista de todos los contenedores de productos
+const productsList = document.querySelector('.products')
+
+//Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar')
+
+const countProducts = document.querySelector('#contador-productos')
+
+productsList.addEventListener('click', e => {
+    if(e.target.classList.contains('btn-add-cart')){
+        const product = e.target.parentElement
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h3').textContent,
+            price: product.querySelector('.price').textContent,
+        };
+
+        const exits = allProducts.some(product => product.title === infoProduct.title)
+
+        if(exits){
+            const products = allProducts.map(product => {
+                if(product.title === infoProduct.title){
+                    product.quantity++;
+                    return product
+                } else{
+                    return product
+                }
+            })
+            allProducts = [...products]
+        }else{
+
+            allProducts = [...allProducts, infoProduct];
+        }
+
+
+        showHTML();
+    }
+});
+
+rowProduct.addEventListener('click', () => {
+    if(e.target.classList.contains('icon-close')){
+        const product = e.target.parentElement
+        const title = product.querySelecor('p').textContent 
+
+        allProducts = allProducts.filter(product => product.title !== title);
+
+        showHTML()
+    }
+})
+
+//Funcion para mostrar HTML
+const showHTML = () => {
+
+    if(!allProducts.length){
+        containerCartProducts.innerHTML=`<p class="cart-empty">El carrito está vacío</p>`
+    }
+
+    //Limpiar HTML
+    rowProduct.innerHTML = '';
+
+    let  total = 0;
+    let totalOfProducts = 0;
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div')
+        containerProduct.classList.add('cart-product')
+
+        containerProduct.innerHTML = `            
+            <div class="cart-item">
+                <span class="fas fa-times"></span>
+                <img src="./img/pan.avif" alt="">
+                <div class="content">
+                    <h3>${product.title}</h3>
+                    <div class="price">${product.price}</div>
+                </div>
+            </div>
+        `
+
+    rowProduct.append(containerProduct);
+
+    total = total + parseInt(product.quantity * product.price.slice(1))
+    totalOfProducts = totalOfProducts + product.quantity;
+
+    });
+
+    valorTotal.innerText = `€${total}`
+    countProducts.innerText = totalOfProducts;
+};
+*/
+
+
+//scroll
+window.onscroll = () =>{
+//  navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+//Scroll navegador
+let ubicacionPrincipal = window.scrollY;
+let $nav = document.querySelector('#nav');
+
+window.addEventListener('scroll', function() {
+    let ubicacionActual = window.scrollY;
+
+    if(ubicacionPrincipal >= ubicacionActual){
+        $nav.style.top = "0px"
+    } else {
+        $nav.style.top = "-150px"
+    }
+
+    ubicacionPrincipal = ubicacionActual
+});
+
+//Fecha nacimiento actualizable cada día: 120 años
+document.addEventListener('DOMContentLoaded', (event) => {
+    const today = new Date().toISOString().split('T')[0];
+    const minDateString = new Date(new Date().setFullYear(new Date().getFullYear() - 120)).toISOString().split('T')[0];
+
+    document.getElementById('edad').setAttribute('max', today);
+    document.getElementById('edad').setAttribute('min', minDateString);
+});
